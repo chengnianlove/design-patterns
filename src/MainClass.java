2dc74faa88afd3491142e233df4c6a2a2fdd5903
程念 <chengnian@www.zhuofansoft.com>
2021-01-23 15:22:11 +0800

    创建项目

---
+++ src/MainClass.java
@@ -0,0 +1,9 @@
+/**
+ * @author create by Niancheng On 2021/1/23 15:15
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
